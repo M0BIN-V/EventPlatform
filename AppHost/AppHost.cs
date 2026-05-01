a74fd327fa5ce8f2787f12a2688a110aed9f5842
Mobin Valikhani <mobinvalikhani@gmail.com>
2026-05-01 16:33:47 +0330

    init

---
+++ AppHost/AppHost.cs
@@ -0,0 +1,28 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder
+    .AddPostgres("event-platform-postgres")
+    .WithImage("postgis/postgis", "latest")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("event-platform-postgres-volume")
+    .WithPgAdmin(config => config
+        .WithContainerName("event-platform-pgadmin")
+        .WithImage("dpage/pgadmin4", "9.9.0")
+        .WithLifetime(ContainerLifetime.Persistent)
+        .WithVolume("event-platform-pgadmin-volume"));
+
+var database = postgres.AddDatabase("event-platform-database");
+
+var api = builder
+    .AddProject<Api>("event-platform-api")
+    .WithReference(database)
+    .WaitFor(database);
+
+var webapp = builder
+    .AddProject<WebApp>("event-platform-webapp")
+    .WithReference(api)
+    .WaitFor(api);
+
+builder.Build().Run();
