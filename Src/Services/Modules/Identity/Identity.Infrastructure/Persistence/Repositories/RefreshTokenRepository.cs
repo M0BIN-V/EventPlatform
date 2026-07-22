42350facc82a0cfbb75505c5eb31a1c094afad88
Mobin Valikhani <mobinvalikhani@gmail.com>
2026-07-22 12:59:48 +0330

    feat: add RefreshTokenRepository for managing refresh tokens in the database

---
+++ Src/Services/Modules/Identity/Identity.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,37 @@
+using Identity.Application.Common.Contracts.Persistence;
+using Identity.Domain.Entities;
+using Identity.Infrastructure.Persistence.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Persistence.Repositories;
+
+public class RefreshTokenRepository(EfIdentityDbContext context) : IRefreshTokenRepository
+{
+    public void Add(RefreshToken refreshToken)
+    {
+        context.RefreshTokens.Add(refreshToken);
+    }
+
+    public async Task<List<RefreshToken>> GetActiveTokensAsync(string userId, CancellationToken ct = default)
+    {
+        return await context.RefreshTokens
+            .Where(t => t.UserId == userId && t.IsActive)
+            .ToListAsync(ct);
+    }
+
+    public void Update(RefreshToken refreshToken)
+    {
+        context.RefreshTokens.Update(refreshToken);
+    }
+
+    public void Update(List<RefreshToken> refreshTokens)
+    {
+        context.RefreshTokens.UpdateRange(refreshTokens);
+    }
+
+    public async Task<RefreshToken?> GetByTokenHashAsync(string tokenHash, CancellationToken ct = default)
+    {
+        return await context.RefreshTokens
+            .FirstOrDefaultAsync(t => t.TokenHash == tokenHash, ct);
+    }
+}
