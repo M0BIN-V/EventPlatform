e6080d89dd2c5ccc76feb645799fbd29be7b614b
Mobin Valikhani <mobinvalikhani@gmail.com>
2026-07-23 15:58:38 +0330

    tmp

---
+++ Identity.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
--- Src/Services/Modules/BuildingBlocks/Application/Handler.cs
+++ Src/Services/Modules/BuildingBlocks/Application/Handler.cs
@@ -1,11 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace BuildingBlocks.Application;
 
-    public abstract class Handler<TRequest>
-    {
-        public abstract Task Handle(TRequest request, CancellationToken ct);
-    }
+public abstract class Handler<TRequest>
+{
+    public abstract Task Handle(TRequest request, CancellationToken ct);
+}
 
-    public abstract class Handler<TRequest, TResponse>
-    {
-        public abstract Task<TResponse> HandleAsync(TRequest request, CancellationToken ct = default );
-    }
+public abstract class Handler<TRequest, TResponse>
+{
+    public abstract Task<TResponse> HandleAsync(TRequest request, CancellationToken ct = default);
+}
