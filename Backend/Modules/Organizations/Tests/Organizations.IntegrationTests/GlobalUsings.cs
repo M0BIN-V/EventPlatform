1fc726392540a4fe5305bfb5a4e734b9e2acf7ee
Mobin Valikhani <mobinvalikhani@gmail.com>
2026-08-12 12:40:36 +0330

    feat: add integration and unit test projects for Organizations module

---
+++ Backend/Modules/Organizations/Tests/Organizations.IntegrationTests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using System.Net;
+global using System.Net.Http.Json;
+global using Organizations.Application.Common.Errors;
+global using Shared.IntegrationTests.Extensions;
+global using Shouldly;
---
+++ Backend/Modules/Organizations/Tests/Organizations.UnitTests/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using FluentValidation;
+global using FluentValidation.Results;
+global using Organizations.Application.Common.Errors;
+global using Organizations.Domain.Entities;
+global using NSubstitute;
+global using Shouldly;
+global using static NSubstitute.Arg;
+global using static NSubstitute.Substitute;
